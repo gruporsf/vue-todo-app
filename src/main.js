edf6d5e1b3db4c83f56de42493b70dc88e6121af
Ricardo <gruporsf@gmail.com>
2021-02-02 15:15:18 -0300

    Primeiro commit da app

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config';
+
+//importação dos componentes
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+
+//importação dos css
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+const app = createApp(App);
+
+//Registra o uso do primevue
+app.use(PrimeVue);
+
+//Registra os componentes
+app.component('Button', Button);
+app.component('InputText', InputText);
+
+app.mount('#app');
